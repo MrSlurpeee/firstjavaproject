56856f4e40f0acad282e90542977f524d0e1d151
ArunG <arun_graeff@stu.indianhills.edu>
2024-11-21 11:20:22 -0600

    javaexample

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Single-line comment in Java
+        /*
+        * Multi-line comment in Java
+        *
+        *
+        */
+       // System.out.println("Arun Graeff says hi");
+
+       // JOptionPane.showMessageDialog(null, "this is a graphical Output");
+        System.out.println("Your random is: " + ((int) (Math.random() * 11)));
+    }
+}
